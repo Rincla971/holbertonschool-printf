aa31cb4f52d667a993ced35ff6dd349badba6ee1
EtienneAbengclear <abengetienne@gmail.com>
2023-11-23 09:27:56 -0800

    Add my function printf

--- _printf.c
+++ _printf.c
@@ -1,55 +1,58 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
 
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ *
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    va_start(args, format);
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-        format++; /* Move past '%' */
-            if (*format == 'c')
-            {
-                /* Print a character */
-                putchar(va_arg(args, int));
-                count++;
-            }
-            else if (*format == 's')
-            {
-                /* Print a string */
-                char *str = va_arg(args, char *);
-                while (*str != '\0')
-                {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*format == '%')
-            {
-                /* Print a '%' character */
-                putchar('%');
-                count++;
-            }
-            else
-            {
-                /* Invalid format specifier, ignore and continue */
-                putchar('%'); /* Print the '%' that was skipped */
-                putchar(*format); /* Print the character after '%'*/
-                count += 2;
-            }
-        }
-        else
-        {
-            /* Normal character, print it */
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return count;
+	char lettre;  /* Declaration and initialization for character */
+	char *string; /* Declaration and initialization for string */
+	char pcent;   /* Declaration and initialization for percentage */
+	va_list ap;   /* Special type in C that manipulates the list of arguments */
+
+	va_start(ap, format); /* Initialize the list to start working with a list of arguments */
+
+	while (*format != '\0') /* Loop to iterate through each character of the format string */
+	{
+		if (*format == '%') /* Check if the current character is '%' */
+		{
+			++format;
+			if (*format == 'c') /* If the format specifier is 'c' */
+			{
+				lettre = va_arg(ap, int); /* Retrieve the character argument */
+				putchar(lettre); /* Print the character using putchar */
+			}
+			else if (*format == 's') /* If the format specifier is 's' */
+			{
+				string = va_arg(ap, char*); /* Retrieve the string argument */
+				while (*string)
+				{
+					putchar(*string); /* Print each character of the string using putchar */
+					string++;
+				}
+			}
+			else if (*format == '%') /* If the format specifier is '%' */
+			{
+				pcent = '%';
+				putchar(pcent); /* Print the percentage using putchar */
+			}
+			else
+			{
+				putchar(*format); /* Print the character without format specifier */
+			}
+		}
+		else
+		{
+			putchar(*format); /* Print the character without format specifier */
+		}
+		++format;
+	}
+	va_end(ap); /* end the processing of optional arguments */
+	return 0; /* return value, modify according to your needs */
 }
